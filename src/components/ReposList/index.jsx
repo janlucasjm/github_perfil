490d8d078a42b3de3602f1b0ef5e556e5f62db3e
Jan Lucas <janlucas13@gmail.com>
2024-01-06 19:40:52 -0300

    adicionando tratações de error ao colocar nome inválido

--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -5,41 +5,63 @@ import styles from './ReposList.module.css';
 const ReposList = ({nomeUsuario}) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [ocorreuError, setOcorreuError] = useState(false);
+
+    const myRequest = new Request(`https://api.github.com/users/${nomeUsuario}/repos`);
+    const messageError = 'Ocorreu um erro ao encontrar o usuário, tente novamente!';
 
     useEffect(() => {
         setEstaCarregando(true)
-        fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json())
+        setOcorreuError(false)
+        fetch(myRequest)
+        .then(res => {
+            if(res.ok) {
+                return res.json();
+            } else {
+                throw new Error ('Algo deu errado');
+            }
+        })
         .then(resJson => {
             setTimeout( () => {
                 setEstaCarregando(false)
                 setRepos(resJson)
             }, 2000);
         })
+        .catch(error => {
+            console.log(error)
+            setOcorreuError(true);
+        })
     }, [nomeUsuario])
 
     return (
-        <div className="container">
-            {estaCarregando ? (
-                <h1 className={styles.itemLoading}></h1>
+        <>
+            
+            {ocorreuError ? (
+                <span className={styles.error}>{messageError}</span>
             ) : (
-            <ul className={styles.list}>
-                {repos.map(({id, name, language, html_url} = repositorio) => (
-                    <li key={id} className={styles.listItem}>
-                        <div className={styles.listName}>
-                        <b>Nome:</b> {name}
-                        </div>
+                <div className="container">
+                    {estaCarregando ? (
+                        <h1 className={styles.itemLoading}></h1>  
+                    ) : (
+                    <ul className={styles.list}>
+                        {repos.map(({id, name, language, html_url} = repositorio) => (
+                            <li key={id} className={styles.listItem}>
+                                <div className={styles.listName}>
+                                <b>Nome:</b> {name}
+                                </div>
 
-                        <div className={styles.listLanguage}>
-                        <b>Linguagem:</b> {language}
-                        </div>
+                                <div className={styles.listLanguage}>
+                                <b>Linguagem:</b> {language}
+                                </div>
 
-                        <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no GitHub</a>
-                    </li>
-                ))}
-            </ul>
+                                <a className={styles.itemLink} target="_blank" href={html_url}>Visitar no GitHub</a>
+                            </li>
+                        ))}
+                    </ul>
+                    )}
+                </div>
             )}
-        </div>
+        </>
     )
 }
 
